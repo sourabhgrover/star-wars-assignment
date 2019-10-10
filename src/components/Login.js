9ebaeca7eb34836b7859a8b5f84e77c74f60fa3c
Sourabh Grover <sourabhgrover29@gmail.com>
2019-10-10 09:24:45 +0530

    Update UI of Login Screen

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,95 +1,33 @@
 import React from "react";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import Typography from "@material-ui/core/Typography";
-import Container from "@material-ui/core/Container";
-import { withStyles } from "@material-ui/core/styles";
-import PropTypes from "prop-types";
-import { Field, reduxForm } from "redux-form";
-
-const styles = theme => ({
-  "@global": {
-    body: {
-      backgroundColor: theme.palette.common.white
-    }
-  },
-  paper: {
-    marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center"
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1)
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2)
-  }
-});
+import "../css/Login.css";
 
 class Login extends React.Component {
   render() {
-    const { classes } = this.props;
     return (
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
+      <div className="main">
+        <p className="sign" align="center">
+          Sign in
+        </p>
+        <form className="form1">
+          <input
+            className="un "
+            type="text"
+            align="center"
+            placeholder="Username"
+          />
+          <input
+            className="pass"
+            type="password"
+            align="center"
+            placeholder="Password"
+          />
+          <button className="submit" align="center">
             Sign in
-          </Typography>
-          <form className={classes.form} noValidate>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Sign In
-            </Button>
-          </form>
-        </div>
-      </Container>
+          </button>
+        </form>
+      </div>
     );
   }
 }
 
-Login.propTypes = {
-  classes: PropTypes.object.isRequired
-};
-
-const styleHOC = withStyles(styles)(Login);
-export default reduxForm({ form: "Login" })(styleHOC);
+export default Login;
